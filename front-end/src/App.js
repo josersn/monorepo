ff26e72fbb5e7b00924d1b6b01874e6ba1e598c6
josersn <nettorammos@hotmail.com>
2022-04-06 19:40:12 -0300

    feat: button component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,12 @@
+import Button from "./components/button";
+
 function App() {
   return (
     <div >
      <h1>Hello World</h1>
+     <Button>
+       Teste
+     </Button>
     </div>
   );
 }
---
+++ front-end/src/components/button/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Button(props) {
+    return (
+        <button>{props.children}</button>
+    );
+}
+
+export default Button;
