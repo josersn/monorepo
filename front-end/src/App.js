babf2ded761670e3512b4a9c7ccafef1baaac268
Vinicius-Mance <vinimanceprof@gmail.com>
2022-04-08 20:44:54 -0300

    rotas a serem implementadas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,18 +4,22 @@ import { NormalizeStyles } from "./shared/NormalizeStyles";
 import Input from "./components/input";
 import Home from "./pages/home";
 import Register from "./pages/register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 function App() {
 
   let userNotLoggedIn;
-  // userNotLoggedIn = true;
-  return (
-    <>
-    <NormalizeStyles/>
-      <Header loggedIn={userNotLoggedIn} pageName="Teste" />
-      {/* <Home/> */}
-      <Register/>
-    </>
-  );
+  userNotLoggedIn = true;
+    return (
+    <Router>
+        <NormalizeStyles />
+        <Header loggedIn={userNotLoggedIn} pageName="Teste" />
+        <Route path="/" element={<Home/>}/>
+    </Router>
+    );
 }
 
 export default App;
