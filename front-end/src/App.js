9660c33431f9bb12630f505082cb36a338e83f26
josersn <nettorammos@hotmail.com>
2022-04-12 22:33:15 -0300

    feat: function to HOC

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,22 +11,30 @@ import Register from "./pages/register";
 import Information from "./pages/informations";
 import Header from "./components/header";
 import About from "./pages/about";
+import { useState } from "react";
 
 function App() {
+
+  const [page, setPage] = useState("");
+
+  function handlePageName(pageName) {
+    setPage(pageName);
+    return pageName;
+  }
 
   let userNotLoggedIn;
   userNotLoggedIn = true;
   return (
     <BrowserRouter>
       <NormalizeStyles />
-      <Header loggedIn={userNotLoggedIn} />
+      <Header loggedIn={userNotLoggedIn} page={page} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/formations" element={<Information pageName="Formação"/>} />
-        <Route path="/experience" element={<Information pageName="Experiência"/>} />
-        <Route path="/hobbies" element={<Information pageName="Hobbies"/>} />
-        <Route path="/aboutme" element={<Information pageName="Sobre mim"/>} />
+        <Route path="/formations" element={<Information pageName={handlePageName("Formação")} />} />
+        <Route path="/experience" element={<Information pageName={handlePageName("Experiência")}/>} />
+        <Route path="/hobbies" element={<Information pageName={handlePageName("Hobbies")}/>} />
+        <Route path="/aboutme" element={<About pageName={handlePageName("Sobre")}/>} />
       </Routes>
     </BrowserRouter>
   );
