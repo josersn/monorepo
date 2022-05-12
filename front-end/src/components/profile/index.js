445ebf622afcb279ac4a23f6d4b1c126c3c74448
josersn <nettorammos@hotmail.com>
2022-05-12 20:46:46 -0300

    second way

--- front-end/src/components/profile/index.js
+++ front-end/src/components/profile/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Container } from './styles';
-import ProfilePic from "assets/images/profilepic.jpg"
 
 function Profile(props) {
   return <Container>
             José Ramos
-            <img src={ProfilePic} alt="José Ramos"/>
+            <img src="assets/images/profilepic.jpg" alt="José Ramos"/>
          </Container>;
 }
 
