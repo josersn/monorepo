dd7a879cc4810264a4800998fc05d1bcbde7d629
josersn <nettorammos@hotmail.com>
2022-05-12 20:13:26 -0300

    remove profile img

--- front-end/src/components/profile/index.js
+++ front-end/src/components/profile/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Container } from './styles';
-import ProfilePic from "../../assets/images/profilepic.jpg"
+// import ProfilePic from "../../assets/images/profilepic.jpg"
 
 function Profile(props) {
   return <Container>
             José Ramos
-            <img src={ProfilePic} alt="José Ramos"/>
+            {/* <img src={ProfilePic} alt="José Ramos"/> */}
          </Container>;
 }
 
