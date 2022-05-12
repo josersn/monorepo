5608a83833579d209ce54d52bf6537a7612d5c90
josersn <nettorammos@hotmail.com>
2022-05-12 20:11:58 -0300

    remove logo

--- front-end/src/components/logo/index.js
+++ front-end/src/components/logo/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Container } from './styles';
-import logo from '../../assets/icons/logo.svg'
+// import logo from '../../assets/icons/logo.svg'
 
 function Logo(props) {
   return (<Container>
-          <img src={logo} alt="ESG"/>
+          {/* <img src={logo} alt="ESG"/> */}
           {!props.showName && <h1>ESG</h1>}
           </Container>);
 }
